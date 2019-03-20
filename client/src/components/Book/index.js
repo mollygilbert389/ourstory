3c348916734c801d77f7e307a74321d0b09fed8c
Molly Gilbert <msgilbert389@gmail.com>
2019-03-19 19:56:57 -0500

    adding some styling

--- client/src/components/Book/index.js
+++ client/src/components/Book/index.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./style.css";
 
 function Book(props) {
-  return <div className="book">This is the Book Div</div>;
+  return <div className="book">
+    <div className="leftside">
+    Left side div
+  </div>
+  <div className="rightside">
+    Right side div
+  </div>
+  </div>;
 }
 
 export default Book;
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function Footer(props) {
+    return <div className="footer"> Here is our footer</div>;
+  }
+  
+  export default Footer;
