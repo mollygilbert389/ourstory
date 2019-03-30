cba5328a02f27fe060210c0afcc401bfcfc27b13
18eddymedina <18emedina@gmail.com>
2019-03-30 15:04:32 -0500

    post works

--- models/userText.js
+++ models/userText.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userTextSchema = new Schema({
-  userID: { type: String, required: true },
+  //userID: { type: String, required: true },
   date: { type: Date, default: Date.now },
   UserText: { type: String, required: true }
 });
