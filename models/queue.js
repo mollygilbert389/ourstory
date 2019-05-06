d19dab440297d6a191ffc926d2cf5e0e581265f3
Shuler <msmehaf2@gmail.com>
2019-05-06 12:58:12 -0500

    checks against a document in db for active user or not to only allow one user at a time

--- models/queue.js
+++ models/queue.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+
 const queueSchema = new Schema({
   active: { type: Boolean, default: false },
   userID: { type: String, defualt: "bob" }
 });
+
 
 const Queue = mongoose.model("Queue", queueSchema);
 
